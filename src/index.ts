95e72f8d3b08124cae172330195a0c80c22d54b2
Fernando Basso <fernandobasso.br@gmail.com>
2020-10-31 14:43:53 -0300

    Update configs for swagger ui and api server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import swaggerUI from 'swagger-ui-express';
-import YAML from 'yamljs';
+// import swaggerUI from 'swagger-ui-express';
+// import YAML from 'yamljs';
 
 import dotenv from 'dotenv';
+
+/**
+ * Loads the key-value pairs from ‘.env’.
+ */
+dotenv.config();
 
 import { skillsRouter } from 'src/routes/skills';
 import { usersRouter } from 'src/routes/users';
@@ -13,19 +18,12 @@ import {
   sessionsPOST,
 } from 'src/controllers/SessionsController';
 
-
 import db from 'src/db';
 
 ////
 // Init the Mongoose/MongoDB Database Connection.
 //
 db();
-
-/**
- * Loads the key-value pairs from ‘.env’.
- */
-dotenv.config();
-
 
 ////
 // Since ‘.dev’ has become part of valid TLDs, (about 2017), we can't
@@ -38,11 +36,6 @@ const PORT = Number(process.env.PORT) || 3001;
 
 const app = express();
 app.use(bodyParser.json());
-
-const swaggerDoc = YAML.load('src/openapi/openapi.yml');
-app.use('/apidocs', swaggerUI.serve, swaggerUI.setup(swaggerDoc, {
-  explorer: true,
-}));
 
 ////
 // Register the routes.
@@ -62,12 +55,6 @@ app.get('/', (req, res) => {
 // Login.
 //
 app.post('/sessions', sessionsPOST);
-
-////
-// To list skills the user must be logged in.
-//
-// app.get('/skills', authToken, skillsIndex);
-
 
 app.listen(PORT, HOST, function listener () {
   console.log(`App listening on http://${HOST}:${PORT}`);
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,49 +1,83 @@
 import express from 'express';
+import { readFileSync } from 'fs';
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 
-/**
- * @TODO: Handle HTTPS too.
- */
+//
+// @TODO: Handle HTTPS too.
+//
 
-/**
- * The spec file ‘openapi.yml’ is served from ‘src/openapi/’ directory,
- * and is publicly accessible from the url path ‘/openapi.yml’.
- *
- * The file is relative to the root url ‘/’, so we just need to pass
- * the path without a leading ‘/’.
- *
- * @TODO: Consider serving this file from another path, or at least make
- * sure we NEVER inadvertently place sensitive stuff in ‘src/openapi/ directory.
- */
+////
+// The spec file ‘openapi.yml’ is served from ‘src/openapi/’ directory,
+// and is publicly accessible from the url path ‘/openapi.yml’.
+//
+// The file is relative to the root url ‘/’, so we just need to pass
+// the path without a leading ‘/’.
+//
+// @TODO: Consider serving this file from another path, or at least make
+// sure we NEVER inadvertently place sensitive stuff in ‘src/openapi/ directory.
+//
 const SWAGGER_OPENAPI_SPEC_FILE_PATH =
   process.env.SWAGGER_OPENAPI_SPEC_FILE_PATH || 'openapi.yml';
 
 
-/**
- * Make sure you have the host bellow, or the one you decided to
- * use in the env var when starting the server in ‘/etc/hosts’. Ex, add
- * this line to ‘/etc/hosts’:
- *
- *   127.0.0.1 swagger.skillsapi.local
- */
+////
+// Make sure you have the host bellow, or the one you decided to
+// use in the env var when starting the server in ‘/etc/hosts’. Ex, add
+// this line to ‘/etc/hosts’:
+//
+//   127.0.0.1 swagger.skillsapi.local
+//
 const SWAGGER_HOST = process.env.SWAGGER_HOST || 'swagger.skillsapi.local';
 
 const SWAGGER_PORT = Number(process.env.SWAGGER_PORT) || 3002;
 
-const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
+////
+// Since we are using ‘swagger-ui-dist’ we need to replace the pets URL with
+// the location of our own ‘openapi.yml’ URL when serving the index.html file.
+//
+// https://github.com/swagger-api/swagger-ui/issues/4624#issuecomment-396439809
+//
+const html = readFileSync(`${pathToSwaggerUi}/index.html`)
+  .toString()
+  .replace(
+    'https://petstore.swagger.io/v2/swagger.json',
+    `http://${SWAGGER_HOST}:${SWAGGER_PORT}/${SWAGGER_OPENAPI_SPEC_FILE_PATH}`,
+  );
 
 const app = express();
+
+////
+// We need to serve the directory where the ‘openapi.yml’ file is located so
+// Swagger UI can fetch and make use of it.
+//
 app.use(express.static('src/openapi'));
+
+////
+// Now we serve the root path _and_ ‘/index.html’ with our new, updated
+// html content that contains the URL to our own ‘openapi.yml’ spec file.
+//
+app.get('/', (req, res) => res.send(html));
+app.get('/index.html', (req, res) => res.send(html));
+
+////
+// Finally serve Swagger UI.
+//
 app.use(express.static(pathToSwaggerUi));
 
+////
+// Our URL string.
+//
 const swaggerUIUrl = String.prototype.concat(
   'http://',
   SWAGGER_HOST,
   ':',
   String(SWAGGER_PORT),
-  '/?url=',
-  SWAGGER_OPENAPI_SPEC_FILE_PATH,
 );
 
+////
+// Start the server at the specified host and port and also log the Swagger UI
+// URL so we know where to point our browsers to.
+//
 app.listen(
   SWAGGER_PORT,
   SWAGGER_HOST,
